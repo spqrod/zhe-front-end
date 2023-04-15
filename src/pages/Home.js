66970d0e87e42b24fcae7f073e5586c50dc6b779
rodionion <rodionvh@gmail.com>
2023-04-15 16:33:43 -0300

    Style methods section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,20 @@ import ConsultationBookingProcess from "../components/ConsultationBookingProcess
 import { Link } from "react-router-dom";
 import image1 from "../images/3.JPG";
 import { useEffect } from "react";
-
+import crystal1 from "../images/crystal-1.png"
+import crystal2 from "../images/crystal-2.png"
+// import crystal3 from "../images/crystal-3.png"
+// import crystal4 from "../images/crystal-4.png"
+// import crystal5 from "../images/crystal-5.png"
+import crystal6 from "../images/crystal-6.png"
+// import crystal7 from "../images/crystal-7.png"
+// import crystal8 from "../images/crystal-8.png"
+import crystal9 from "../images/crystal-9.png"
+// import crystal10 from "../images/crystal-10.png"
+// import crystal11 from "../images/crystal-11.png"
+// import crystal12 from "../images/crystal-12.png"
+import crystal13 from "../images/crystal-13.png"
+ 
 export default function Home({consultationPrice, consultationLength, whatsAppLink}) {
     useEffect(() => {
 
@@ -99,35 +112,45 @@ export default function Home({consultationPrice, consultationLength, whatsAppLin
                 <h2>Методы работы</h2>
                 <div className="methodsContainer">
                     <div className="methodContainer">
-                        <PhotoOutlinedIcon className="methodIcon" />
+                        <div className="crystalImageContainer">
+                            <img className="crystalImage" src={crystal1} alt="" />
+                        </div>
                         <div className="methodInfoContainer">
                             <h3>Клиент-центрированная терапия</h3>
                             <p>Направление созданное К. Роджерсом на основе принципов гуманистической психологии. Психолог в консультировании ведет себя не директивно, рефлексивно по отношению к клиенту.  Главный принцип это «здесь и теперь». Нет никаких правил, не указаний и четного алгоритма к действию. Клиент самостоятельно решает свой запрос, но опираясь на принятие, доверие  и опыт специалиста.</p>
                         </div>
                     </div>
                     <div className="methodContainer">
-                        <PhotoOutlinedIcon className="methodIcon" />
+                        <div className="crystalImageContainer">
+                            <img className="crystalImage" src={crystal6} alt="" />
+                        </div>
                         <div className="methodInfoContainer">
                             <h3>Метод дерефлексии</h3>
                             <p>Снятие излишнего самоконтроля, чрезмерного самокопания. В некоторых случаях это не является продуктивным, а отнимает только силы и энергию. </p>
                         </div>
                     </div>
                     <div className="methodContainer">
-                        <PhotoOutlinedIcon className="methodIcon" />
+                        <div className="crystalImageContainer">
+                            <img className="crystalImage" src={crystal2} alt="" />
+                        </div>
                         <div className="methodInfoContainer">
                             <h3>Метод пародоксальной интенции</h3>
                             <p>Подведение клиента к тому, что вызывает страхи  и большее стремление  избежать. Благодаря этому достигается ощущение свободы. Прежние тревожности и страхи недосказывают такого негативного воздействия, что позволяет двигаться перед. </p>
                         </div>
                     </div>
                     <div className="methodContainer">
-                        <PhotoOutlinedIcon className="methodIcon" />
+                        <div className="crystalImageContainer">
+                            <img className="crystalImage" src={crystal9} alt="" />
+                        </div>
                         <div className="methodInfoContainer">
                             <h3>Арт-терапия</h3>
                             <p>Это направление в психотерапии, метод психологической коррекции, в основе которого лежит использование искусства и творчества. Своеобразное связующее звено между сознанием и подсознанием человека. Часто ее называют мостом, проложенным между разумом и душой.</p>
                         </div>
                     </div>
                     <div className="methodContainer">
-                        <PhotoOutlinedIcon className="methodIcon" />
+                        <div className="crystalImageContainer">
+                            <img className="crystalImage" src={crystal13} alt="" />
+                        </div>
                         <div className="methodInfoContainer">
                             <h3>Позитивная психология</h3>
                             <p>Отрасль психологической практики, в центре которой находится позитивный потенциал человека. Этот подход нацелен  оптимизацию функционирования человека, поиск факторов, которые способствовали бы благополучному существованию и расцвету индивида.</p>
