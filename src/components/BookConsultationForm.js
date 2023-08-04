846df95232c348e3d3e6d1250397824f9b838a31
rodionion <rodionvh@gmail.com>
2023-08-04 17:50:41 -0300

    Migrate logic to OO model

--- src/components/BookConsultationForm.js
+++ src/components/BookConsultationForm.js
@@ -2,7 +2,7 @@ import SocialLinks from "../components/SocialLinks";
 import DatePicker from "react-datepicker";
 import { Link } from "react-router-dom";
 import ReCAPTCHA from "react-google-recaptcha";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import ru from 'date-fns/locale/ru';
 import { useState } from "react";
 import "../styles/bookConsultationForm.css";
@@ -12,85 +12,125 @@ export default function BookConsultationForm() {
     const today = new Date();
     const tomorrow = new Date(today).setDate(today.getDate() + 1);
     const todayPlus30Days = new Date(today).setDate(today.getDate() + 30);
+
     const captchaRef = useRef(null);
 
-    const availableDays = [
-        new Date(2023, 7, 27, 11, 0),
-        new Date(2023, 7, 27, 12, 0),
-        new Date(2023, 7, 27, 13, 30),
-
-        new Date(2023, 7, 28, 9, 0),
-        new Date(2023, 7, 28, 10, 0),
-        new Date(2023, 7, 28, 11, 0),
-        new Date(2023, 7, 28, 11, 30),
-        
-        new Date(2023, 7, 29, 10, 0),
-        
-        new Date(2023, 7, 2, 9, 0),
-        new Date(2023, 7, 7, 9, 0),
-        new Date(2023, 7, 15, 9, 0),
-    ];
-
     const [selectedDate, setSelectedDate] = useState();
-
     const [availableTimes, setAvailableTimes] = useState([]);
+    const [availableDates, setAvailableDates] = useState();
+    const [dataFromDB, setDataFromDB] = useState([]);
 
-    function updateAvailableTimes(selectedDate) {
-        let newArray = [];
-        newArray = availableDays.filter(date => 
-            ((date.getFullYear() === selectedDate.getFullYear()) 
-            && (date.getMonth() === selectedDate.getMonth()) 
-            && (date.getDate() === selectedDate.getDate()))
-        );
-        setAvailableTimes(newArray);
-    }
+    useEffect(() => {
+        api.getAvailableDatesFromDB();
+    }, []);
 
-    function handleDateSelect(selectedDate) {
-        updateAvailableTimes(selectedDate);
-    }
+    const api = {
+        getAvailableDatesFromDB: function() {
+            fetch("/api/dates")
+                .then(response => response.json())
+                .then(dataFromDBFetch => {
+                    setDataFromDB([...dataFromDBFetch]);
+                    const temporaryArray = [];
+    
+                    dataFromDBFetch.forEach(itemFromDBFetch => {
+                        let dateItem = new Date(itemFromDBFetch.date);
+                        dateItem = controller.correctDateUsingTimezoneOffset(dateItem);
+                        temporaryArray.push(dateItem);
+                    });
+    
+                    setAvailableDates(temporaryArray);
+                });
+        },
+        updateDateAsTakenInDB: function(date) {
 
-    function handleDateChange(date) {
-        setSelectedDate(date)
-    }
-
-    async function handleSubmit(e) {
-        e.preventDefault();
-        const token = captchaRef.current.getValue();
-        captchaRef.current.reset();
-
-        const { date, name, email, phone, message } = e.target.elements;
-
-        const formData = {
-            date: date.value,
-            name: name.value,
-            email: email.value,
-            phone: phone.value,
-            message: message.value
         }
+    };
 
-        const response = await fetch("https://website.com/book-consultation", {
-            method: "POST",
-            body: JSON.stringify(formData)
-        });
-
-        const result = await response.json();
-        alert(result.status);
+    const controller = {
+        getDateIDFromDB: function(dateFromForm) {
+            const document = dataFromDB.find(item => controller.formatDateToMatchTheForm(item.date) === dateFromForm);
+            console.log(document._id);
+            return document._id;
+        },
+        handleDateSelect: function(selectedDate) {
+            display.updateAvailableTimes(selectedDate);
+        },
+        handleSubmit: async function(e) {
+            e.preventDefault();
+            // const token = captchaRef.current.getValue();
+            // captchaRef.current.reset();
+    
+            const { date, name, email, phone, message } = e.target.elements;
+    
+            const formData = {
+                date: date.value,
+                // name: name.value,
+                // email: email.value,
+                // phone: phone.value,
+                // message: message.value
+            }
+            
+            // console.log(dataFromDB);
+            // console.log(`date from DB ${availableDates}`);
+            // console.log(`formData.date = ${new Date(formData.date)}`);
+            // const formattedDate = formatDateFromFormToISOUTCString(formData.date);
+            // console.log(formattedDate);
+            controller.getDateIDFromDB(formData.date);
+            // updateDateAsTakenInDB(formattedDate);
+    
+            // const response = await fetch("https://website.com/book-consultation", {
+            //     method: "POST",
+            //     body: JSON.stringify(formData)
+            // });
+    
+            // const result = await response.json();
+            // alert(result.status);
+    
+        },
+        formatDateToMatchTheForm: function(date) {
+            date = controller.correctDateUsingTimezoneOffset(date);
+            const options = {
+                dateStyle: "short",
+                timeStyle: "short",
+            }
+            const formattedDate = new Date(date).toLocaleString("ru-RU", options);
+            return formattedDate;
+        },
+        handleDateChange: function(date) {
+            setSelectedDate(date)
+        },
+        handleTelInputChange: function() {
+            // revealConsentCheckboxAndReCaptcha();
+        },
+        correctDateUsingTimezoneOffset: function(date) {
+            date = new Date(date);
+            date = date.getTime() + date.getTimezoneOffset() * 1000 * 60;
+            date = new Date(date);
+            return date;
+        }
     }
 
-    function handleTelInputChange() {
-        revealConsentCheckboxAndReCaptcha();
-    }
-
-    function revealConsentCheckboxAndReCaptcha() {
-        const legalConsentCheckboxContainer = document.querySelector(".legalConsentCheckboxContainer");
-        legalConsentCheckboxContainer.classList.add("active");
-        const reCaptcha = document.querySelector(".reCaptcha");
-        reCaptcha.classList.add("active");
+    const display = {
+        updateAvailableTimes: function(selectedDate) {
+            let newArray = [];
+            newArray = availableDates.filter(date => 
+                ((date.getFullYear() === selectedDate.getFullYear()) 
+                && (date.getMonth() === selectedDate.getMonth()) 
+                && (date.getDate() === selectedDate.getDate()))
+            );
+            setAvailableTimes(newArray);
+        },
+        revealConsentCheckboxAndReCaptcha: function() {
+            const legalConsentCheckboxContainer = document.querySelector(".legalConsentCheckboxContainer");
+            legalConsentCheckboxContainer.classList.add("active");
+            const reCaptcha = document.querySelector(".reCaptcha");
+            reCaptcha.classList.add("active");
+        }
     }
 
     return (
         <div className="bookConsultationForm">
-            <form className="form" onSubmit={handleSubmit}>
+            <form className="form" onSubmit={controller.handleSubmit}>
 
                 {/* ADD IDs TO INPUT FIELDS */}
 
@@ -99,24 +139,24 @@ export default function BookConsultationForm() {
                     withPortal
                     minDate={tomorrow}
                     maxDate={todayPlus30Days}
-                    includeDates={availableDays}
+                    includeDates={availableDates}
                     includeTimes={availableTimes}
                     locale={ru}
                     showTimeSelect
                     timeFormat="p"
                     placeholderText="Выбрать день"
-                    onSelect={handleDateSelect}
+                    onSelect={controller.handleDateSelect}
                     selected={selectedDate}
-                    onChange={handleDateChange}
-                    dateFormat="dd MMMM yyyy, h:mm"
+                    onChange={controller.handleDateChange}
+                    dateFormat="dd.MM.yyyy, HH:mm"
                     timeCaption="Время (по МСК)"
                     id="date"
                 />
                 <input className="inputField" type="text" placeholder="Имя" name="name" id="name" />
-                <input className="inputField" type="email" placeholder="E-mail" name="email" required />
-                <input className="inputField" type="tel" placeholder="Телефон" name="phone" required onChange={handleTelInputChange} />
+                <input className="inputField" type="email" placeholder="E-mail" name="email"  />
+                <input className="inputField" type="tel" placeholder="Телефон" name="phone"  onChange={controller.handleTelInputChange} />
                 <div className="legalConsentCheckboxContainer">
-                    <input type="checkbox" className="legalConsentCheckbox" name="legalConsentCheckbox" id="legalConsentCheckbox" required />
+                    <input type="checkbox" className="legalConsentCheckbox" name="legalConsentCheckbox" id="legalConsentCheckbox"  />
                     <label htmlFor="legalConsentCheckbox">Я принимаю <Link to="/terms-of-service">Условия использования</Link> и <Link to="/privacy-policy">Политику конфиденциальности</Link></label>
                 </div>
                 {/* <ReCAPTCHA 
