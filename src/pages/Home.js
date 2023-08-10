8fea3da1fa0a011fab10c4bb716b72837c26af04
rodionion <rodionvh@gmail.com>
2023-08-10 13:30:31 -0300

    Add mobile styling for forms

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -281,6 +281,18 @@ export default function Home({consultationPrice, consultationLength, whatsAppLin
                 <h2>Записаться на консультацию</h2>
                 <BookConsultationForm />
             </section>
+
+            <div className="container">
+                <div className="box one"></div>
+                <div className="box two"></div>
+                <div className="box three"></div>
+                <div className="box four"></div>
+                <div className="box five"></div>
+                <div className="box six"></div>
+                <div className="box seven"></div>
+
+
+            </div>
         </main>
     );
 }
